b755fe9e26e0a775586880f8d7ba012bab0170fd
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2019-11-04 14:06:51 -0400

    creando vistta de cliente

---
+++ cliente/src/app/content/content.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import {ChartsModule,Label} from 'ng2-charts';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+
+export class ContentComponent implements OnInit {
+
+    // Pie
+    public pieChartOptions: ChartOptions= {
+      responsive: true,
+      legend: {
+        position: 'top',
+      },
+      plugins: {
+        datalabels: {
+          formatter: (value, ctx) => {
+            const label = ctx.chart.data.labels[ctx.dataIndex];
+            return label;
+          },
+        },
+      }
+    };
+    public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], ['Mail Sales']];
+    public pieChartData: number[] = [300, 500, 100];
+    public pieChartType: ChartType = 'pie';
+    public pieChartLegend = true;
+    // public pieChartPlugins = [pluginDataLabels];
+    public pieChartColors = [
+      {
+        backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+      },
+    ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
