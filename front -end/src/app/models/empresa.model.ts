de95699e663ea290b97ddb1f35a3c864ac0b823f
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2019-11-07 12:01:31 -0400

    cree los model de empresa,categoria,compra,venta,producto,usuario,

---
+++ front -end/src/app/models/empresa.model.ts
@@ -0,0 +1,7 @@
+export class Empresa {
+  public idEmpresa: number;
+  public nombre: string;
+  public telefono: string;
+  public direccion: string;
+  public logo: string;
+}
---
+++ front -end/src/app/models/producto.model.ts
@@ -0,0 +1,10 @@
+export class Producto {
+  public idProducto: number;
+  public idCategoria: string;
+  public idEmpresa: string;
+  public foto: string;
+  public descripcion: string;
+  public precio: string;
+  public minimoCompra: string;
+  public cantidad: string;
+}
