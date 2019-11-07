d5f790233f6603d489ec64f2ddf057e70eaeda7a
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2019-11-07 15:14:04 -0400

    se ha ruteado con respectivos componentes

--- front -end/src/app/pages/pages.module.ts
+++ front -end/src/app/pages/pages.module.ts
@@ -10,6 +10,12 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {ProgressComponent} from './progress/progress.component';
 import {Graficas1Component} from './graficas1/graficas1.component';
 import {FormsModule} from '@angular/forms';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { ProductoComponent } from './producto/producto.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CompraComponent } from './compra/compra.component';
+import { VentaComponent } from './venta/venta.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +23,12 @@ import {FormsModule} from '@angular/forms';
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
+    EmpresaComponent,
+    ProductoComponent,
+    CategoriaComponent,
+    CompraComponent,
+    VentaComponent,
+    UsuarioComponent,
   ],
   exports: [// para que de puedan acceder desde cualquier logar
     PagesComponent,
--- front -end/src/app/shared/shared.module.ts
+++ front -end/src/app/shared/shared.module.ts
@@ -3,20 +3,25 @@ import { BreadcumbsComponent } from './breadcumbs/breadcumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
-    declarations: [
-        BreadcumbsComponent,
-        HeaderComponent,
-        SidebarComponent,
-        NopagefoundComponent
-    ], exports: [
-        BreadcumbsComponent,
-        HeaderComponent,
-        SidebarComponent,
-        NopagefoundComponent
-    ]
+  declarations: [
+    BreadcumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NopagefoundComponent
+  ],
+  imports: [
+    RouterModule
+  ],
+  exports: [
+    BreadcumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NopagefoundComponent
+  ]
 })
 export class SharedModule { }
