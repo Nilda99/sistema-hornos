0a1ee1a1ef07b6f6f1b56a0fc453e68bfd6ddbaa
Maeba <orlando_guery@hotmail.com>
2019-12-04 13:54:47 -0400

    ya estan las relaciones  me faltaba en  las migraciones

--- back-end/migrations/20191111182938-create-producto.js
+++ back-end/migrations/20191111182938-create-producto.js
@@ -1,45 +1,58 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Productos', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      IdCategoria: {
-        type: Sequelize.INTEGER
-      },
-      Foto: {
-        type: Sequelize.STRING
-      },
-      Descripcion: {
-        type: Sequelize.STRING
-      },
-      Precio: {
-        type: Sequelize.INTEGER
-      },
-      MinimoCompra: {
-        type: Sequelize.INTEGER
-      },
-      Cantidad: {
-        type: Sequelize.INTEGER
-      },
-      IdEmpresa: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Productos');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Productos', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            IdCategoria: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "Categoria",
+                    key: "id"
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE"
+
+            },
+            Foto: {
+                type: Sequelize.STRING
+            },
+            Descripcion: {
+                type: Sequelize.STRING
+            },
+            Precio: {
+                type: Sequelize.INTEGER
+            },
+            MinimoCompra: {
+                type: Sequelize.INTEGER
+            },
+            Cantidad: {
+                type: Sequelize.INTEGER
+            },
+            IdEmpresa: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "empresa",
+                    key: "id"
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE"
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Productos');
+    }
 };
--- back-end/migrations/20191111183631-create-venta.js
+++ back-end/migrations/20191111183631-create-venta.js
@@ -1,27 +1,33 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Venta', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      IdProducto: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Venta');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Venta', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            IdProducto: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "Producto",
+                    key: "id"
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE"
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Venta');
+    }
 };
